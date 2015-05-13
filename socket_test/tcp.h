4a71d11e66c08a71f77da0ad61e35a58c40fc3bc
root <root@debian>
2015-05-13 17:27:36 +0200

    Ajout des fichiers pour TCP

---
+++ socket_test/tcp.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+//Structure du header TCP
+#include <netinet/tcp.h>
+
+struct in_addr  getIp(char* interface);
