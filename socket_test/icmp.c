ed44a8f244690ae7da968145e8caacb203af4378
Tomviaud <tomviaud@hotmail.fr>
2015-05-27 22:41:00 +0200

    avancement de icmp

--- socket_test/icmp.c
+++ socket_test/icmp.c
@@ -2,16 +2,20 @@
 #include "ip.h"
 #include "icmp.h"
 
-void makeICMP_header(struct icmphdr *icmp,char datagram[4096],u_int8_t typeICMP)
+void makeICMP_header(struct icmphdr *icmp,u_int8_t typeICMP)
 {
 
 	char *fake_datagram;
 	//On remplit le paquet
 	icmp->type = typeICMP;
 	icmp->code = 0;
-	//icmp->checksum = checksum( 
+	//On met le checksum à 0 avant de le calculer
+	icmp->checksum = 0;
+	//Valeur au hasard permettant de distinguer différent paquet ICMP
 	icmp->un.echo.id = 0;
+	//On met la séquence à 0
 	icmp->un.echo.sequence = 0;
-	
-	//int size_datagram_cs = sizeof(struct tcphdr) + sizeof(struct icmphdr) + strlen(data);
+
+	//On calcule le checksum
+	icmp->checksum = checksum((unsigned short*)icmp, sizeof(struct icmphdr));
 }
