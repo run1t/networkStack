2a5e318e9eec8e827b5176df48d9807861494fd6
Tomviaud <tomviaud@hotmail.fr>
2015-06-02 09:52:56 +0200

    Push partiel ICMP

--- socket_test/icmp.c
+++ socket_test/icmp.c
@@ -14,6 +14,7 @@ void makeICMP_header(struct icmphdr *icmp,u_int8_t typeICMP,int seq_number,int i
 	icmp->un.echo.id = htons(rand());
 
 	if(typeICMP == ICMP_ECHOREPLY){
+		printf("seq: %d\n",seq_number);
 		icmp->un.echo.sequence = htons(seq_number + 1);
 		icmp->un.echo.id = id_number;
 	}else if(typeICMP == ICMP_ECHO){
@@ -29,7 +30,7 @@ void sendICMP_request(struct icmphdr *ICMP_received,int type_ICMP)
 	//struct pour le socket
 	struct sockaddr_in sin;
 	sin.sin_family = AF_INET;
-	sin.sin_addr.s_addr = inet_addr("10.17.19.135");
+	sin.sin_addr.s_addr = inet_addr("10.17.19.85");
 
 	//On ouvre un socket
 	int sd;
