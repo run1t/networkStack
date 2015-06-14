e42215e77552ce3fccca85504a31ddb4de34d6dd
Tomviaud <tomviaud@hotmail.fr>
2015-06-14 17:55:40 +0200

    Ajout de doc Server.cpp

--- lib/Server.cpp
+++ lib/Server.cpp
@@ -8,6 +8,12 @@
 //
 #include "Server.h"
 Server* Server::server;
+
+/**
+ * \fn void Server::onDatas(Connection *connection)
+ * \brief Fonction permettant de réagir sur la réception de Data
+ * \param Connection *connection
+ */
 void onDatas(Connection *connection){
 	cout << "on a notre premier niveau "<< endl;
 	Server::server->onData(connection);
@@ -18,6 +24,12 @@ void onDatas(Connection *connection){
 * Gestion de la stack IP
 */
 
+/**
+ * \fn Server::Server(string ip, int port)
+ * \brief Constructeur de la classe Server
+ * \param ip ip du serveur
+ * \param port port du serveur
+ */
 Server::Server(string ip, int port){
  	this->stacker =  new Stack(ip,port);
     this->stacker->addDataEvent(onDatas);
@@ -30,14 +42,19 @@ Server::Server(string ip, int port){
 * de notre pile
 */
 
-
-
-
+/**
+ * \fn void Server::addEventData  (function<void(Connection*)> func)
+ * \brief bind une fonction à l'évenement de réception de Data
+ * \param function fonction à bindé
+ */
 void Server::addEventData  (function<void(Connection*)> func){
 	this->onData = func;
 }
 
-
+/**
+ * \fn void Server::join()
+ * \brief lance l'écoute
+ */
 void Server::join(){
 	Server::server = this;
 	this->stacker->receiver();
